911af7220944631e14a7c94b38e399f6d23530d1
bldfrstr <lesnikovdaniel@gmail.com>
2024-02-11 05:02:08 +0300

    About hello world gui

---
+++ gui/index.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+def window():
+   app = QApplication(sys.argv)
+   widget = QWidget()
+
+   def button1_clicked():
+      print("Button 1 clicked")
+   def button2_clicked():
+      print("Button 2 clicked")
+
+
+   textLabel = QLabel(widget)
+   textLabel.setText("Yandex Music Desktop App by bldfrstr")
+   textLabel.move(20,10)
+
+   button1 = QPushButton(widget)
+   button1.setText("Button1")
+   button1.move(64,32)
+   button1.clicked.connect(button1_clicked)
+
+   button2 = QPushButton(widget)
+   button2.setText("Button2")
+   button2.move(64,64)
+   button2.clicked.connect(button2_clicked)
+
+
+   widget.setGeometry(50,50,320,200)
+   widget.setWindowTitle("PyQt5 Example")
+   widget.show()
+   sys.exit(app.exec_())
+
+if __name__ == '__main__':
+   window()
